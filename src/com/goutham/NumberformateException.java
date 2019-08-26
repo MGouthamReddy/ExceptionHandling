7327cc1ea6fe17171859a0c49abc7f23210f7c3e
MGouthamReddy <reddygoutham142@gmail.com>
2019-08-26 11:40:05 +0530

    Goutham

---
+++ src/com/goutham/NumberformateException.java
@@ -0,0 +1,17 @@
+package com.goutham;
+
+public class NumberformateException {
+
+	public static void main(String[] args) {
+         System.out.println("This statement will be executed");
+       try {
+         Integer it=new Integer("ABC");
+       }
+        catch(Exception e) { 
+       System.out.println("a");
+       
+       {
+       System.out.println("This statement will be not executed");
+	}
+
+}}}
